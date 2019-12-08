6492ac8cb02d04e5ed8f21805b2cf2a4cd206885
BOUBIDI abdesselem borhane eddine <36478447+mrAbdou@users.noreply.github.com>
2019-12-08 21:13:57 +0100

    updated

--- TSP/TSP.m
+++ TSP/TSP.m
@@ -4,6 +4,7 @@ nbrVilles = 5;
 nbrSolution = 6;
 maxDistance = 50;
 nbrPaires = 2;
+nbrIterations = 10;
 prob_mutation = 0.09;
 prob_croisement = 0.01;
 population = init(nbrSolution,nbrVilles);
@@ -13,19 +14,24 @@ mat_adj = [0 5 3 0 0;
            0 8 7 0 2;
            0 0 0 2 0];
 [lignes, colonnes] = size(population);
+k = 0;
+while nbrIterations > 0
 %evaluation : ----------------------------
 for i = 1 : lignes
     F = fitness( population(i,:), mat_adj);
    population(i, colonnes + 1) = F;
 end
+k = k+1;
+minFitness(k) = min(population(:, colonnes + 1));
 %----------------croisement--------------------
 pop_sel = selection(population, nbrVilles, nbrSolution, nbrPaires);
-pop_sel =pop_sel(:,1:size(pop_sel,2) - 1);
+pop_sel =pop_sel(:,1:size(pop_sel,2) - 1)
 [lignes, colonnes] = size(pop_sel);
 for i = 1 : 2 : lignes - 1
 [fils1,fils2] = croisement(pop_sel(i,:),pop_sel(i+1,:),prob_croisement);
-pop_cr(i,:) = fils1;
-pop_cr(i+1,:) = fils2;
+
+pop_cr(i,:) = removeDuplicated(fils1);
+pop_cr(i+1,:) = removeDuplicated(fils2);
 end
 pop_cr
 [lignes, colonnes] = size(pop_cr);
@@ -34,3 +40,8 @@ for i = 1 : lignes
 pop_mut(i,:) = mutation(pop_cr(i,:),prob_mutation);
 end
 pop_mut
+
+population = remplacement(pop_mut,pop_sel,mat_adj,nbrPaires )
+nbrIterations = nbrIterations - 1;
+end
+min(minFitness);
---
+++ TSP/remplacement.m
@@ -0,0 +1,11 @@
+function [ pop ] = remplacement(population, fils,mat_adj, nbrPaires)
+pop1 = [population;fils];
+[lignes, colonnes] = size(pop1);
+for i = 1 : lignes
+    F = fitness( pop1(i,:), mat_adj);
+   pop1(i, colonnes + 1) = F;
+end
+pop1 = sortrows(pop1, -size(pop1,2));
+pop = pop1(1:2*nbrPaires,1:5);
+end
+
